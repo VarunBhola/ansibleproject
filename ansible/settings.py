bd96354a8e53506c0ce6d0fc9dcf071b90fcb7e4
VarunBhola <Varun40569@yahoo.co.in>
2016-02-19 13:18:54 +0530

    Update settings.py

--- ansible/settings.py
+++ ansible/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ansiledb',
+        'NAME': 'ansibledemo',
         'USER': 'testuser',
         'PASSWORD': 'test123456',
         'HOST': 'localhost',
