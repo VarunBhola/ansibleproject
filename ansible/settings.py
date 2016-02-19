ddcc23af3577c217630592cabbca235df5e17a8b
Varun Bhola <varun.bhola@nsi.lab>
2016-02-19 12:43:50 +0530

    chnage

--- ansible/settings.py
+++ ansible/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ansiledb',
         'USER': 'postgres',
-        'PASSWORD': '123456',
+        'PASSWORD': 'v123456',
         'HOST': 'localhost',
         'PORT': '5432',
     }
